ce9f03180c92edb0ba6d70f4f1c8f6794c292dc9
Vitaliy Bobrov <bobrov1989@gmail.com>
2018-12-09 23:21:12 +0100

    Cleanup

--- src/app/path-resolve.service.ts
+++ src/app/path-resolve.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
 
 import { paths } from './app-paths';
 
